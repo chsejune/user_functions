20ea26ffd9d9a95a274d62acd3a8bb7c1c6f9184
sjcLab <chsejune@naver.com>
2018-01-16 11:24:52 +0900

    image_read.py

---
+++ image_read.py
@@ -0,0 +1,17 @@
+__author__ = 'Sejune Cheon'
+__version__ = '1.0'
+
+import scipy
+import numpy as np
+
+
+def imread(path, is_grayscale = True):
+    """
+    :param path: 불러들일 이미지 파일의 위치+파일명 입력
+    :param is_grayscale: 이미지가 흑백인지 컬러인지 여부
+    :return: numpy float 형태로 한장의 이미지를 출력해준다
+    """
+    if (is_grayscale):
+        return scipy.misc.imread(path, flatten = True).astype(np.float)
+    else:
+        return scipy.misc.imread(path, mode='RGB').astype(np.float)
