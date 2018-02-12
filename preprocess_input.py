0482a2eb87bd3f64a3dacf8f184ca3fbd671b081
sjcLab <chsejune@naver.com>
2018-02-12 15:23:34 +0900

    preprocess_input.py __version__ = '1.1'

--- preprocess_input.py
+++ preprocess_input.py
@@ -1,17 +1,17 @@
 __author__ = 'Sejune Cheon'
-__version__ = '1.0'
+__version__ = '1.1'
 __environment__ = 'Python-3.5.2'
 __description__ = 'developed for preprocessing feature to image-wise data-form'
 
 import numpy as np
 
-def preprocess_feature_input(output_features):
+def preprocess_feature_input(output_features, output_shape):
 
     feature_images = []
     for img in output_features:
         feature_img = []
         for val in img.flatten():
-            feature_img.append(np.full((150, 150), val))
+            feature_img.append(np.full(output_shape, val))
         feature_images.append(np.rollaxis(np.array(feature_img), 0, 3))
 
     return np.array(feature_images)
